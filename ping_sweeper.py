f63b8739626d2277c8231ce389a78c2d070f1516
aashishramchand <49671862+aashishramchand@users.noreply.github.com>
2020-07-11 16:15:52 -0400

    Update and rename ping_swpr.py to ping_sweeper.py

--- ping_sweeper.py
+++ ping_sweeper.py
@@ -4,32 +4,12 @@ logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 import sys
 
-# dst_ip = input("Enter IP Range(a.b.c.d-y): ")
-# # dst_ip= "192.168.204.229-230"
-#
-# res = dst_ip.split("-")
-# # print(res)
-# # ntwrk_id = res[0][:-1]
-# temp = res[0].split(".")
-# ntwrk_id = ".".join(temp[:-1])
-# #print(ntwrk)
-#
-# host_begin = (temp[-1:])
-# host_begin = int(host_begin[0])
-# # print(host_begin)
-# #print(host_beg)
-# #exit(0)
-#
-# host_end = int(res[1])
-# # print "net- ", ntwrk_id
-# # print "hostbeg - ",host_begin , " -- ", host_end
-
 def alive_check(target):
     res = sr1(IP(dst=target)/ICMP(), timeout=10, verbose=0)
     if res is not None:
-        print(target, " --> Host is Up.")
+        print(target, " ===> is Up.")
     else:
-        print(target, " --> Host is Down.")
+        print(target, " --> is Down.")
 
 def main():
     # print(len(sys.argv))
